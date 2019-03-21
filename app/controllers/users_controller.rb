d1a38e2ebb5459e20a591297d9da729c98d18925
sddanila <sddanila@yahoo.co.uk>
2019-03-21 16:25:38 +0000

    add user signup to models and controllers - user validations are working!

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,20 @@
 class UsersController < ApplicationController
+  
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to [:products]
+    else
+      render :new
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
-  validates :first_name, presence: true, length: { minimum: 1 }
-  validates :last_name, presence: true, length: { minimum: 1 }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX}, uniqueness: { case_sensitive: false}
   has_secure_password
