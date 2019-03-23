8bdf260dff6c7f25ad6a8b65a1d474c902654bba
sddanila <sddanila@yahoo.co.uk>
2019-03-23 02:35:55 +0000

    add delete button to reviews if the user is currently signed in

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,28 @@
 class ReviewsController < ApplicationController
+  before_filter :check_logged_in
   
   def create
-    @product = Product.find_by_id(params[:id])
-    @reviews = Review.where(product_id: params[:id])
+    @review = Review.new(allowed_params)
+    @review.user_id = current_user.id
+    @review.save!
+    redirect_to products_path(@review.products)
   end
 
+  def destroy
+    review = Review.find(params[:id])
+    product = Product.find(params[:product_id])
+    review.destroy!
+    redirect_to products_path
+  end
+
+  def check_logged_in
+    if !current_user
+      redirect_to login_path
+    end
+  end
+
+  private
+    def allowed_params
+      params.require(:review).permit(:rating, :description, :product_id, :user_id)
+    end
 end
